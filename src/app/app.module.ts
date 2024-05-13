30de353451bf69b2e5524660a6b6b328bc91c0a0
Ons Hadrich <ounshadriche@gmail.com>
2024-05-13 16:33:15 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Layouts/navbar/navbar.component';
+import { FooterComponent } from './Layouts/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
