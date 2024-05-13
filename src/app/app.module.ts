1b6f8d562e5dbb34830a25efcd7601e2f4590376
Ons Hadrich <ounshadriche@gmail.com>
2024-05-13 17:46:31 +0300

    Set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './Layouts/navbar/navbar.component';
 import { FooterComponent } from './Layouts/footer/footer.component';
 import { HomeComponent } from './home/home.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
