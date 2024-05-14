274648e5b072c105640279f426ed71c2f6de2422
Ons Hadrich <ounshadriche@gmail.com>
2024-05-14 13:41:02 +0300

    Add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,26 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import { SearchComponent } from './search/search.component';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { ModalSearchComponent } from './modal-search/modal-search.component';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    SearchComponent
+    SearchComponent,
+    ModalSearchComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
