436f9a0f947df2338f2865c3f8f1c86441e0c12a
Ons Hadrich <ounshadriche@gmail.com>
2024-05-14 18:27:01 +0300

    Set up

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ModalSearchComponent } from '../modal-search/modal-search.component';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent {
+export class SearchComponent  implements OnInit{
+  constructor(public dialog: MatDialog) {}
+  ngOnInit(): void {
 
+  }
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(ModalSearchComponent, {
+      width: '450px',
+
+      enterAnimationDuration,
+      exitAnimationDuration,
+    });
+  }
 }
