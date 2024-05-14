900835d9271bfa3ecb5a9912c4994b51ff4e34d2
Ons Hadrich <ounshadriche@gmail.com>
2024-05-14 13:41:11 +0300

    Create new component

---
+++ src/app/modal-search/modal-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-search',
+  templateUrl: './modal-search.component.html',
+  styleUrls: ['./modal-search.component.css']
+})
+export class ModalSearchComponent {
+
+}
