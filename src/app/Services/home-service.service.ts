07c7bed68ffa960789db7728675baf35e8cd6838
Ons Hadrich <ounshadriche@gmail.com>
2024-06-09 02:04:38 +0300

    add new service

--- src/app/Services/home-service.service.ts
+++ src/app/Services/home-service.service.ts
@@ -4,21 +4,21 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { FileLike } from '@bytescale/upload-widget';
 import { FileInput } from 'ngx-material-file-input';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HomeServiceService {
-
   private apiUrl = 'http://localhost:5000/predict';
+  private apiUrlGenerative = 'http://localhost:5000/generate';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   predict(image: File): Observable<any> {
     const formData = new FormData();
     formData.append('file', image);
-    console.log(image)
-    return this.http.post<any>(this.apiUrl, formData).pipe(
-      catchError(this.handleError)
-    );
+    console.log(image);
+    return this.http
+      .post<any>(this.apiUrl, formData)
+      .pipe(catchError(this.handleError));
   }
   private handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
@@ -27,10 +27,15 @@ export class HomeServiceService {
     } else {
       // The backend returned an unsuccessful response code.
       console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
     }
     // Return an observable with a user-facing error message.
     return throwError('Something bad happened; please try again later.');
+  }
+  generate(text: any): Observable<any> {
+    return this.http
+      .post<any>(this.apiUrlGenerative, text)
+      .pipe(catchError(this.handleError));
   }
 }
